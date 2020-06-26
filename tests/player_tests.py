337121604c66856ff5d426285e2300df7bfab5fe
Sasi Kumar T <sasikt@thoughtworks.com>
2020-06-26 10:08:12 +0530

    Rename test methods as per python convention

--- tests/player_tests.py
+++ tests/player_tests.py
@@ -2,12 +2,13 @@ import unittest
 from src.player import Player
 
 class PlayerTests(unittest.TestCase):
-     def should_initialize_player_with_name_symbol(self):
+
+     def test_initialize_player_with_name_symbol(self):
         player = Player("Sasi", "X")
         self.assertEquals(player.name, "Sasi")
         self.assertEquals(player.symbol, "X")
 
 
-     def should_tell_a_position_for_symbol_placement(self):
+     def test_tell_a_position_for_symbol_placement(self):
          player = Player("Sasi", "X")
          self.assertEquals(player.getNextPosition(), (2,3))
--- tests/tic_tac_board_tests.py
+++ tests/tic_tac_board_tests.py
@@ -3,30 +3,30 @@ from src.tic_tac_board import TicTacBoard
 
 class TicTacToeTests(unittest.TestCase):
 
-    def should_create_an_empty_tictactoe_board(self):
-        board = TicTacBoard()
+    def test_create_an_empty_tictactoe_board(self):
+        board = TicTacBoard(3)
         self.assertIsNotNone(board)
 
-    def should_validate_move_position(self):
+    def test_validate_move_position(self):
         #Todo move this to setup
         board = TicTacBoard()
         isValid = board.place_position("X", (0,-1))
         self.assertFalse(isValid)
 
-    def should_place_item_in_position_if_valid(self):
+    def test_place_item_in_position_if_valid(self):
         board = TicTacBoard()
         isValid = board.place_position("X", (1,1))
         self.assertTrue(isValid)
 
-    def should_check_if_game_is_over(self):
+    def test_check_if_game_is_over(self):
         board = TicTacBoard()
         self.assertTrue(board.is_game_over())
 
-    def should_return_false_if_game_is_not_over(self):
+    def test_return_false_if_game_is_not_over(self):
         board = TicTacBoard()
         self.assertFalse(board.is_game_over())
 
 
-    def should_return_open_positions_for_game(self):
+    def test_return_open_positions_for_game(self):
         board = TicTacBoard()
         self.assertEqual(board.open_positions(), [(1,1), (2,2)])
