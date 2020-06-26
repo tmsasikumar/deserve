9a589a0fd34cec5bf5a60e78e3935e6e6a1df672
Sasi Kumar T <sasikt@thoughtworks.com>
2020-06-26 10:58:23 +0530

    Implemented open positions and place position methods'

--- src/tic_tac_board.py
+++ src/tic_tac_board.py
@@ -1,5 +1,23 @@
 class TicTacBoard():
-    def __init__(self):
-        self.board = ( ['-','-', '-'], ['-','-', '-'], ['-','-', '-'])
+    def __init__(self, n):
+        self.size = n
+        items = ["-"]*n
+        self.board = [items]*n
+
+    def place_position(self, position, symbol):
+        if position > self.size or position < 0 or self.board[position[0]][position[1]] != "-":
+            return False
+        else:
+           self.board[position[0]][position[1]] = symbol
+           return True
+
+
+    def open_positions(self):
+        open_positions = []
+        for i in self.size:
+            for j in self.size:
+                if self.board[i][j] == "-":
+                    open_positions.append((i,j))
+        return open_positions
 
 
