ff001d442c7780123edac7412e429bbfd2140682
Sasi Kumar T <sasikt@thoughtworks.com>
2020-06-26 09:56:38 +0530

    Added more tests for player and board

---
+++ src/player.py
@@ -0,0 +1,6 @@
+class Player():
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+
--- tests/tic_tac_board_tests.py
+++ tests/tic_tac_board_tests.py
@@ -1,8 +1,5 @@
 import unittest
-
-from src import tic_tac_board
 from src.tic_tac_board import TicTacBoard
-
 
 class TicTacToeTests(unittest.TestCase):
 
@@ -10,23 +7,26 @@ class TicTacToeTests(unittest.TestCase):
         board = TicTacBoard()
         self.assertIsNotNone(board)
 
-
     def should_validate_move_position(self):
         #Todo move this to setup
         board = TicTacBoard()
-        isValid = board.placePosition("X", (0,-1))
+        isValid = board.place_position("X", (0,-1))
         self.assertFalse(isValid)
 
     def should_place_item_in_position_if_valid(self):
         board = TicTacBoard()
-        isValid = board.placePosition("X", (1,1))
+        isValid = board.place_position("X", (1,1))
         self.assertTrue(isValid)
 
-
     def should_check_if_game_is_over(self):
-        board = TicTacBoard
-        self.assertTrue(board.isGameOver())
+        board = TicTacBoard()
+        self.assertTrue(board.is_game_over())
 
     def should_return_false_if_game_is_not_over(self):
-        board = TicTacBoard
-        self.assertFalse(board.isGameOver())
+        board = TicTacBoard()
+        self.assertFalse(board.is_game_over())
+
+
+    def should_return_open_positions_for_game(self):
+        board = TicTacBoard()
+        self.assertEqual(board.open_positions(), [(1,1), (2,2)])
