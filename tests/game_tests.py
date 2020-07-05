15db91144eed2de22fb0892d7bba4f03c5f06982
Sasi Kumar T <sasikt@thoughtworks.com>
2020-07-05 16:49:15 +0530

    Remove unused code; fix failing tests

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.Constants import X_SYMBOL, X_WINS, O_WINS, DRAW
+from src.difficulty_level import Difficult_level
 from src.game import Game
 from src.tic_tac_board import TicTacBoard
 
@@ -8,12 +9,12 @@ class GameTests(unittest.TestCase):
 
     def test_check_if_game_is_over(self):
         board = TicTacBoard(3)
-        game = Game(board, difficulty_level)
+        game = Game(board,Difficult_level.MEDIUM)
         self.assertFalse(game.is_over())
 
     def test_game_over_if_diagnol_continous_block(self):
         board = TicTacBoard(3)
-        game = Game(board, difficulty_level)
+        game = Game(board, Difficult_level.MEDIUM)
         board.place_position((0,0), "X")
         board.place_position((1,1), "X")
         board.place_position((2,2), "X")
@@ -21,7 +22,7 @@ class GameTests(unittest.TestCase):
 
     def test_game_over_if_reverse_diagnol_continous_block(self):
         board = TicTacBoard(3)
-        game = Game(board, difficulty_level)
+        game = Game(board, Difficult_level.MEDIUM)
         board.place_position((1,1), "X")
         board.place_position((0,2), "X")
         board.place_position((2,0), "X")
@@ -29,7 +30,7 @@ class GameTests(unittest.TestCase):
 
     def test_game_over_if_straight_continous_blocks(self):
         board = TicTacBoard(3)
-        game = Game(board, difficulty_level)
+        game = Game(board, Difficult_level.MEDIUM)
         board.place_position((0,0), "O")
         board.place_position((1,0), "O")
         board.place_position((2,0), "O")
@@ -37,7 +38,7 @@ class GameTests(unittest.TestCase):
 
     def test_game_over_if_straight_continous_blocks(self):
         board = TicTacBoard(3)
-        game = Game(board, difficulty_level)
+        game = Game(board, Difficult_level.MEDIUM)
         board.place_position((0,0), "O")
         board.place_position((0,1), "O")
         board.place_position((0,2), "O")
@@ -45,7 +46,7 @@ class GameTests(unittest.TestCase):
 
     def test_game_failure_if_straight_continous_blocks(self):
         board = TicTacBoard(3)
-        game = Game(board, difficulty_level)
+        game = Game(board, Difficult_level.MEDIUM)
         board.place_position((0,0), "O")
         board.place_position((0,1), "X")
         board.place_position((0,2), "O")
